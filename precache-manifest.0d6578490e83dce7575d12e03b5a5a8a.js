8753b92db700ee861b8d7ab949745e3f39e10a65
PCU <PCU@PCUs-Mac-mini.local>
2020-06-25 09:33:08 -0400

    Updates

---
+++ precache-manifest.0d6578490e83dce7575d12e03b5a5a8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bd5576cdd6e92631a4412a2fc76476",
+    "url": "/krystafur-toyota-research-dashboard/index.html"
+  },
+  {
+    "revision": "142be8253a2933504b8a",
+    "url": "/krystafur-toyota-research-dashboard/static/css/main.6fd86e75.chunk.css"
+  },
+  {
+    "revision": "e0bd9bb36542d07bebaa",
+    "url": "/krystafur-toyota-research-dashboard/static/js/2.170a3475.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/krystafur-toyota-research-dashboard/static/js/2.170a3475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142be8253a2933504b8a",
+    "url": "/krystafur-toyota-research-dashboard/static/js/main.bb9a5e4c.chunk.js"
+  },
+  {
+    "revision": "f3f79e37195d8f8b8e44",
+    "url": "/krystafur-toyota-research-dashboard/static/js/runtime-main.8f5695f4.js"
+  }
+]);
